8fccc84e40db526e9fd61ab9e29e04b95b971957
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-07-09 17:49:45 +0200

    Fix an exception on shutdown when auto backups are not enabled

--- src/main/java/com/artillexstudios/axinventoryrestore/schedulers/AutoBackupScheduler.java
+++ src/main/java/com/artillexstudios/axinventoryrestore/schedulers/AutoBackupScheduler.java
@@ -5,21 +5,31 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class AutoBackupScheduler {
+    private static ScheduledExecutorService executor = null;
     private static ScheduledFuture<?> future = null;
 
     public static void start() {
-        if (future != null) future.cancel(true);
+        if (future != null) {
+            future.cancel(true);
+        }
 
-        if (!AxInventoryRestore.CONFIG.getBoolean("automatic-backup.enabled")) return;
+        if (!AxInventoryRestore.CONFIG.getBoolean("automatic-backup.enabled")) {
+            return;
+        }
+
+        if (executor == null) {
+            executor = Executors.newSingleThreadScheduledExecutor();
+        }
         final int backupMinutes = AxInventoryRestore.CONFIG.getInt("automatic-backup.minutes", 5);
         final int backupSeconds = AxInventoryRestore.CONFIG.getInt("automatic-backup.seconds", 5);
         final int backupTime = Math.max(1, Math.max(backupMinutes * 60, backupSeconds));
 
-        future = Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+        future = executor.scheduleAtFixedRate(() -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 AxInventoryRestore.getDB().saveInventory(player, "AUTOMATIC", null);
             }
@@ -27,6 +37,12 @@ public class AutoBackupScheduler {
     }
 
     public static void stop() {
-        future.cancel(true);
+        if (future != null) {
+            future.cancel(true);
+        }
+
+        if (executor != null) {
+            executor.shutdown();
+        }
     }
 }
