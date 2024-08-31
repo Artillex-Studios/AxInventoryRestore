151334bf475fc5950c3f632b05693882cdadaa84
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-08-31 12:15:41 +0200

    fix: move clean to a different method so we can better isolate things on profilers

--- src/main/java/com/artillexstudios/axinventoryrestore/database/impl/Base.java
+++ src/main/java/com/artillexstudios/axinventoryrestore/database/impl/Base.java
@@ -631,6 +631,10 @@ public abstract class Base implements Database {
             log.error("An unexpected error occurred while removing last save for {}!", uuid, exception);
         }
 
+        clean(amount);
+    }
+
+    private void clean(int amount) {
         final String sql2 = "DELETE FROM axir_storage WHERE id not IN (SELECT inventoryId FROM axir_backups WHERE inventoryId IS NOT NULL LIMIT ?);";
         try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(sql2)) {
             stmt.setInt(1, amount);
